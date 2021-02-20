8e09c7eee18fe25678a1150820ce0e75b775aa10
Denis Avakov <denis.avakov@pm.me>
2021-02-20 15:58:55 +0300

    feat: add base modules to express

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import compress from 'compression';
+import helmet from 'helmet';
 import routes from '../API/routes/v1';
 
 const app = express();
+
+// gzip compression
+app.use(compress());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
 app.use('/api', routes);
 
 export default app;
